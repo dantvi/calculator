aab2482df0565df85f10e63af2c50b4cc468ecfc
dantvi <daniel.tving@gmail.com>
2024-08-02 00:40:29 +0200

    reset display

--- script.js
+++ script.js
@@ -18,3 +18,11 @@ inputBtns.forEach((inputBtn) => {
         inputBtn.addEventListener('click', () => sendNumberValue(inputBtn.value));
     }
 });
+
+// Reset display
+function resetAll() {
+    calculatorDisplay.textContent = '0';
+}
+
+// Event Listener
+clearBtn.addEventListener('click', resetAll);
